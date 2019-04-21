9481b9262f06983a606cc4e0a80a1f58b52408dd
rmesnard <gruno@free.fr>
2019-04-21 10:38:30 +0200

    Update start-snips.sh

--- start-snips.sh
+++ start-snips.sh
@@ -107,10 +107,10 @@ echo "snips services started.. check logs"
 if [ $ENABLE_INTERCOM == yes ]; then
 	echo "Start intercom"
 	cd /usr/share/snips/skills
-	nohup python3 -u listerner.py 2> /var/log/listerner.log &
-	snips_audio_client=$!
+	nohup python3 -u listener.py 2> /var/log/listerner.log &
+	snips_listener_pid=$!
 	nohup python3 -u doorbell.py 2> /var/log/doorbell.log &
-	snips_doorbell=$!	
+	snips_doorbell_pid=$!	
 fi
 
 echo "running ok"
